221dc3a92077ca1ef33430ee78410a021f15e355
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-10 15:21:13 +0000

    Wed Jan 10 15:21:13 GMT 2018

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -3,20 +3,32 @@ package befaster.solutions;
 import befaster.runner.SolutionNotImplementedException;
 
 public class FizzBuzz {
+    
+    private static Boolean findNumber(int number){
+        
+        while(number != 0){
+            if(number % 10 == number){
+                return true;
+            }
+            number/=10;
+        }
+        
+        return false;
+    }
 
     public static String fizzBuzz(Integer number) {
             String output = "";
-            if(number % 3 == 0){
+            if(number % 3 == 0 || findNumber(3) == true){
                 output+="fizz";
             }
             
             if(output.length() > 0){ 
-                if(number % 5 == 0){
+                if(number % 5 == 0 || findNumber(5) == true){
                     output+=" buzz";
                 }
-            } else if(number % 5 == 0){
+            } else if(number % 5 == 0 || findNumber(5) == true){
                     output+="buzz";
-                }
+            }
                 
             if(output.length() == 0){
                 output+=number;
