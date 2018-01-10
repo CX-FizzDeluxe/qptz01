95a275fa93f7080f82aca6d49b6d34614387db9c
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-10 15:14:01 +0000

    Wed Jan 10 15:14:01 GMT 2018

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -5,21 +5,19 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
-        for(int i = 1; i<=100; ++i){
             String output = "";
-            if(i % 3 == 0){
+            if(number % 3 == 0){
                 output+="fizz ";
             }
             
-            if(i % 5 == 0){
+            if(number % 5 == 0){
                 output+="buzz";
             }
             
             if(output.length() == 0){
-                output+=i;
+                output+=number;
             }
             return output;
-        }
     }
 
 }
